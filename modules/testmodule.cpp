738a6e897360208ba1963a77e87b40d90d86a52a
cold-rivers-snow <1580753591@qq.com>
2019-03-17 20:28:58 +0800

    2

---
+++ modules/testmodule.cpp
@@ -0,0 +1,22 @@
+#include "../src/dso.h"
+#include <stdio.h>
+
+extern int handle(void* data)
+{
+    printf("%s",(char*)data);
+    return MODULE_OK;
+}
+
+extern int init(Module* module)
+{
+    module = &mod;
+    return MODULE_OK;
+}
+
+Module mod{
+    MODULE_MAHOR_VERSION,
+    MODULE_SUB_VERSION,
+    __FILE__,
+    init,
+    handle
+}
